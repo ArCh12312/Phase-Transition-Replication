bb041bb9f98c6accdcdaa72e3b91db4e63ddf19d
Aryan Chowdhury <aryan.chowdhury@student.manchester.ac.uk>
2024-08-13 06:32:37 +0100

    Complete ipynb to run the script and plot the graph

--- run_script.py
+++ run_script.py
@@ -1,11 +1,6 @@
 from DPLL import DPLLSolver
 from SatGen import generate_3sat_instance
 
-# solver = DPLLSolver()
-# problem = generate_3sat_instance(20,90)
-# solver.solve(problem)
-# print(f"Number of Recursive DPLL Calls: {solver.dpll_count}")
-
 def dpll_run_script(num_vars, num_clauses, num_iterations):
     num_calls = []
     for i in range(num_iterations):
@@ -15,14 +10,15 @@ def dpll_run_script(num_vars, num_clauses, num_iterations):
         num_calls.append(solver.dpll_count)
     return num_calls
 
-num_vars = 20
+num_vars = 60
 num_iterations = 100
 
 calls_list = []
 for i in range(2,9):
     num_clauses = num_vars*i
     num_calls = dpll_run_script(num_vars, num_clauses, num_iterations)
-    calls_list.append(num_calls)
+    average = sum(num_calls) / len(num_calls)
+    calls_list.append(average)
 
 print(calls_list)
     
